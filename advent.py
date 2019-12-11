49191f82023a341f6f249bedbfe95c8f2b5c7e7c
pragmaticTNT <pragmaticTNT@gmail.com>
2019-12-11 09:22:09 -0500

    Days 9~10

--- advent.py
+++ advent.py
@@ -1,4 +1,6 @@
 import fileinput
+import itertools as it
+import math
 
 class Node():
     def __init__(self, value):
@@ -34,6 +36,17 @@ class Tree():
             ##print(n, nPtr.orbit)
             nNodes += nPtr.orbit
         return nNodes
+    def sPath(self, src, tar):
+        srcPath = [self.nodeList[src]]
+        tarPath = [self.nodeList[tar]]
+        while srcPath[-1] != self.head:
+            srcPath.append(srcPath[-1].parent)
+        while tarPath[-1] != self.head:
+            tarPath.append(tarPath[-1].parent)
+        while srcPath[-1] == tarPath[-1]:
+            srcPath.pop(-1)
+            tarPath.pop(-1)
+        return len(srcPath + tarPath)
 
 def fuelRequired(mass):
     fuel = mass//3 - 2
@@ -42,6 +55,93 @@ def fuelRequired(mass):
         fuel = fuel//3 - 2
         totalFuel += fuel
     return totalFuel
+
+def getAddr(tape, mode, rel, c):
+    val = 0
+    if mode == 0:
+        val = tape[c]
+    elif mode == 1:
+        val = c
+    elif mode == 2:
+        val = rel + tape[c]
+    return val
+
+def getVal(tape, mode, rel, c):
+    return tape[getAddr(tape, mode, rel, c)]
+
+def intMachine(tape, inputs):
+    c       = 0
+    rel     = 0
+    iCounter= 0
+    while tape[c] != 99:
+        instr = tape[c]
+        mode    = instr // 100
+        instr   = instr % 100
+        if instr == 1:
+            t1 = getVal(tape, mode%10, rel, c+1)
+            mode //= 10
+            t2 = getVal(tape, mode%10, rel, c+2)
+            mode //= 10
+            addr = getAddr(tape, mode, rel, c+3)
+            tape[addr] = t1 + t2
+            c += 4
+        if instr == 2:
+            t1 = getVal(tape, mode%10, rel, c+1)
+            mode //= 10
+            t2 = getVal(tape, mode%10, rel, c+2)
+            mode //= 10
+            addr = getAddr(tape, mode, rel, c+3)
+            tape[addr] = t1 * t2
+            ##print("Op2:", t1, t2, tape[c+3])
+            c += 4
+        if instr == 3:
+            addr = getAddr(tape, mode, rel, c+1)
+            ##print("Get input:", mode, rel, addr)
+            tape[addr] = inputs[iCounter]
+            iCounter += 1
+            c += 2
+        if instr == 4:
+            s = getVal(tape, mode, rel, c+1)
+            c += 2
+            print(s)
+        if instr == 5:
+            s = getVal(tape, mode%10, rel, c+1)
+            mode //= 10
+            t = getVal(tape, mode, rel, c+2)
+            c = t if s else (c + 3)
+        if instr == 6:
+            s = getVal(tape, mode%10, rel, c+1)
+            mode //= 10
+            t = getVal(tape, mode, rel, c+2)
+            c = t if s == 0 else (c + 3)
+        if instr == 7:
+            s = getVal(tape, mode%10, rel, c+1)
+            mode //= 10
+            t = getVal(tape, mode%10, rel, c+2)
+            mode //= 10
+            addr = getAddr(tape, mode, rel, c+3)
+            tape[addr] = s < t
+            ##print("Op7:", s, t)
+            c += 4
+        if instr == 8:
+            s = getVal(tape, mode%10, rel, c+1)
+            mode //= 10
+            t = getVal(tape, mode%10, rel, c+2)
+            mode //= 10
+            addr = getAddr(tape, mode, rel, c+3)
+            tape[addr] = s == t
+            c += 4
+        if instr == 9:
+            rel += getVal(tape, mode, rel, c+1)
+            c += 2
+    return
+
+def formatTape(fileName):
+    tape = []
+    with fileinput.input(fileName) as f:
+        code = f.readline()
+        tape = [int(c) for c in code.split(",")]
+    return tape
 
 def day1(fileName):
     fuel    = 0
@@ -53,82 +153,9 @@ def day1(fileName):
             fuel += addFuel
     print("Total Fuel: ", fuel)
 
-def day2(fileName):
-    with fileinput.input(fileName) as f:
-        code = f.readline()
-        tape = [int(c) for c in code.split(",")]
-    ##print(tape)
-
-    c = 0
-    while tape[c] != 99:
-        instr = tape[c]
-        ##print(instr)
-        mode  = -1
-        if instr > 99:
-            mode    = instr // 100
-            instr   = instr % 100
-        if instr == 1:
-            if mode < 0:
-                tape[tape[c+3]] = tape[tape[c+1]] + tape[tape[c+2]]
-            else:
-                t1 = tape[c+1] if mode % 10 else tape[tape[c+1]]
-                mode = mode // 10
-                t2 = tape[c+2] if mode % 10 else tape[tape[c+2]]
-                tape[tape[c+3]] = t1 + t2
-            c += 4
-        if instr == 2:
-            if mode < 0:
-                tape[tape[c+3]] = tape[tape[c+1]] * tape[tape[c+2]]
-            else:
-                t1 = tape[c+1] if mode % 10 else tape[tape[c+1]]
-                mode = mode // 10
-                t2 = tape[c+2] if mode % 10 else tape[tape[c+2]]
-                tape[tape[c+3]] = t1 * t2
-            c += 4
-        if instr == 3:
-            tape[tape[c+1]] = 5
-            c += 2
-        if instr == 4:
-            if mode < 0:
-                print(tape[tape[c+1]])
-            else:
-                s = tape[c+1] if mode % 10 else tape[tape[c+1]]
-                print(s)
-            c += 2
-        if instr == 5:
-            if mode < 0:
-                c = tape[tape[c+2]] if tape[tape[c+1]] else (c + 3)
-            else:
-                s = tape[c+1] if mode % 10 else tape[tape[c+1]]
-                mode = mode // 10
-                t = tape[c+2] if mode % 10 else tape[tape[c+2]]
-                c = t if s else (c + 3)
-        if instr == 6:
-            if mode < 0:
-                c = tape[tape[c+2]] if tape[tape[c+1]] == 0 else (c + 3)
-            else:
-                s = tape[c+1] if mode % 10 else tape[tape[c+1]]
-                mode = mode // 10
-                t = tape[c+2] if mode % 10 else tape[tape[c+2]]
-                c = t if s == 0 else (c + 3)
-        if instr == 7:
-            if mode < 0:
-                tape[tape[c+3]] = tape[tape[c+1]] < tape[tape[c+2]]
-            else:
-                s = tape[c+1] if mode % 10 else tape[tape[c+1]]
-                mode = mode // 10
-                t = tape[c+2] if mode % 10 else tape[tape[c+2]]
-                tape[tape[c+3]] = s < t
-            c += 4
-        if instr == 8:
-            if mode < 0:
-                tape[tape[c+3]] = tape[tape[c+1]] == tape[tape[c+2]]
-            else:
-                s = tape[c+1] if mode % 10 else tape[tape[c+1]]
-                mode = mode // 10
-                t = tape[c+2] if mode % 10 else tape[tape[c+2]]
-                tape[tape[c+3]] = s == t
-            c += 4
+## ---> No longer works due to changes to intMachine
+##def day2(fileName):
+##    intMachine(formatTape(fileName), [5])
 
 def day3(fileName):
     with fileinput.input(fileName) as f:
@@ -247,14 +274,117 @@ def day6(fileName):
     with fileinput.input(fileName) as f:
         for line in f:
             system.append(line.rstrip().split(")"))
-    ##print(system)
     systemTree = Tree("COM", system)
-    print(systemTree.nOrbits())
+    print(systemTree.sPath("YOU", "SAN") - 2)
+
+def day7(fileName):
+    init    = [i for i in range(5,10)]
+    n       = len(init)
+    tape    = formatTape(fileName)
+    perm    = it.permutations(init)
+    maxAmp  = 0
+
+    for inputs in perm:
+        amp     = 0
+        ptrs    = [0 for i in range(n)]
+        tapes   = [tape[:] for i in range(n)]
+        c       = 0
+        loop    = False
+        end     = False
+        while not end:
+            io = [amp] if loop else [inputs[c], amp]
+            amp, ptr = intMachine(tapes[c], io, ptrs[c])
+            ptrs[c] = ptr
+            c = (c+1) % 5
+            if c == 0:
+                loop = True
+                if tapes[4][ptr] == 99:
+                    end = True
+        if amp > maxAmp:
+            maxAmp = amp
+    print(maxAmp)
+
+def day8(fileName):
+    s = ""
+    with fileinput.input(fileName) as f:
+        s = f.readline()
+        s = s.strip()
+    n = len(s)//150
+    l = [s[150*i:150*i+150] for i in range(n)]
+    ##zeroCount = [row.count('0') for row in l]
+    ##rowIndex = zeroCount.index(min(zeroCount))
+    ##print(l[rowIndex].count('1')*l[rowIndex].count('2'))
+    img = list(l[0])
+    for i in range(150):
+        layer = 0
+        while img[i] == '2':
+            img[i] = l[layer][i]
+            layer += 1
+    img = ['.' if c == '0' else '1' for c in img]
+    for row in [img[25*i:25*i+25] for i in range(6)]:
+        print("".join(row))
+
+def day9(fileName):
+    tape = formatTape(fileName)
+    tape += [0 for i in range(len(tape)*100)]
+    intMachine(tape, [2])
+
+def countAstroid(sector, row, col):
+    width   = len(sector)
+    length  = len(sector[0])
+    tempMap = [[True for i in range(length)] for j in range(width)]
+    for c in range(col+1, length):
+        if sector[row][c] >= 0:
+            sector[row][col] += 1
+            sector[row][c] += 1
+            break
+    for r in range(row+1, width):
+        for c in range(length):
+            if tempMap[r][c] and sector[r][c] >= 0:
+                sector[row][col] += 1
+                sector[r][c] += 1
+                rowGap = r - row
+                colGap = c - col
+                rcGcd = math.gcd(rowGap, colGap)
+                rowGap //= rcGcd
+                colGap //= rcGcd
+                rr = r
+                cc = c
+                while 0 <= rr < width and 0 <= cc < length:
+                    tempMap[rr][cc] = False
+                    rr += rowGap
+                    cc += colGap
+
+def pSector(sector):
+    for row in sector:
+        for cell in row:
+            print('.' if cell<0 else cell, end="")
+        print()
+    print()
+
+def day10(fileName):
+    sector = []
+    with fileinput.input(fileName) as f:
+        for line in f:
+            line = line.strip()
+            sector.append([-1 if c=='.' else 0 for c in line])
+    for row in range(len(sector)):
+        for col in range(len(sector[0])):
+            if sector[row][col] >= 0:
+                countAstroid(sector, row, col)
+                ##pSector(sector)
+    maxRow  = [max(row) for row in sector]
+    rInd    = maxRow.index(max(maxRow))
+    cInd    = sector[rInd].index(max(rInd))
+    ##print(max(sum(sector, [])))
+
+def day11(fileName):
+    return
 
 def main():
-    fileName = "day6-input.txt"
-    ##fileName = "test.txt"
-    day6(fileName)
+    fileName = "day10-input.txt"
+    fileName = "test.txt"
+    day10(fileName)
 
 if __name__ == "__main__":
     main()
